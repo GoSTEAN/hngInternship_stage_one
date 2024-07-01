de8b638f067368819170b49b9053929c6b272ec2
Stephanie Nwankwo <stephybelle208@gmail.com>
2024-07-01 14:07:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# HNG INTERNSHIP STAAGE ONE
+
+## TASK
+
+Create an API endpoint that responds with the client's IP address, location, city temperature
+The API endpoint accepts a visitor_name query parameter /api/hello?visitor_name
+The API responds with the following response object/body
+{
+  "client_ip": // The IP address,
+  "location": // The location,
+  "greeting": "Hello // Your name !, the temperature is //Your Temperature degrees Celsius in // Location"
+}
---
+++ main.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config(); // Load environment variables from .env file
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const ipKey = process.env.IP_API_KEY;
+
+async function getIp(req,res) {
+    try {
+        const {visitor_name} = req.query
+        
+        const ipInfo = await fetch(`https://ipinfo.io/json?token=${ipKey}`);
+        const info = await ipInfo.json();
+        const {ip,city,loc} = info;
+        const [lat,lon] = loc.split(",");
+
+        const weatherInfo = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m`);
+        const weather = await weatherInfo.json();
+        const temp = weather.hourly.temperature_2m[0];
+        const greeting = `Hello ${visitor_name}!, the temperature is ${temp} degrees Celsius in ${city}`;
+        
+        return res.status(200).json({client_ip:ip,location:city,greeting});
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({error: error.message});
+    }
+    
+}
+
+app.get('/api/hello', getIp)
+
+app.listen(PORT, () => console.log('listening on port' + PORT));
