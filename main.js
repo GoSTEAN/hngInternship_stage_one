a5ee6a22e6ef14ec1ae833d999bfd7a2e987ff7f
Stephanie Nwankwo <stephybelle208@gmail.com>
2024-07-01 15:45:29 +0100

    Modify some files

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
 const express = require('express');
-const axios = require('axios');
 require('dotenv').config(); // Load environment variables from .env file
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-const ipKey = process.env.IP_API_KEY;
+const PORT = process.env.PORT || 3000;
 
 async function getIp(req,res) {
     try {
+        const xp = (req.headers['x-forwarded-for'] || '').split(',')[0];
+        
         const {visitor_name} = req.query
 
-        // const ipInfo = await fetch(`https://ipinfo.io/json?token=${ipKey}`);
-        // const info = await ipInfo.json();
-        // const {ip,city,loc} = info;
-        // const [lat,lon] = loc.split(",");
-
-        const zig = await fetch(`https://ipapi.co/${req.ip}/json/`);
+        const zig = await fetch(`https://ipapi.co/${xp}/json/`);
         const data = await zig.json();
         const {ip, city, latitude, longitude} = data;
-        // return res.status(200).json(data);
+
         const weatherInfo = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`);
         const weather = await weatherInfo.json();
         const temp = weather.hourly.temperature_2m[0];
